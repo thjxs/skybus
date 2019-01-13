9ab17d7eabb449bdf43ff697652ae01ab3a9a8a8
thjxs <tanhongjianx@163.com>
2019-01-13 15:25:10 +0000

    Thread factory

--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -3,7 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Thread::class, function (Faker $faker) {
+    $node_ids = [1,2,3];
     return [
-        //
+        'title' => $faker->realText($faker->numberBetween(10,20)),
+        'published_at' => now(),
+        'node_id' => $faker->randomElement($node_ids)
     ];
 });
