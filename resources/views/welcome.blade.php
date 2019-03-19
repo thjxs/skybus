50692a211c473201ca96cddc17494a842dad422f
thjxs <tanhongjianx@163.com>
2019-03-19 13:30:05 +0000

    Remove font-awsome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'spa')</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
+    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}"> -->
     @yield('styles')
 </head>
 <body>
